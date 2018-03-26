969511a1b0ac7965c27a37f2130af16f0e8f702b
Jordi Sala Morales <jordism91@gmail.com>
2018-03-26 18:18:22 +0200

    Add drush sites, first attempt

---
+++ drupal-vm/provisioning/roles/app/templates/aliases.drushrc.php
@@ -0,0 +1,12 @@
+<?php
+
+$aliases['_local'] = [
+  'root' => '/vagrant/web',
+];
+
+$aliases['development'] = [
+  'uri' => 'http://192.168.1.219',
+  'remote-host' => '192.168.1.219',
+  'remote-user' => 'runroom',
+  'root' => '/home/runroom/drupal/current/web',
+];
