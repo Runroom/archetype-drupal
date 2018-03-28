d6f83e05cab6d1f66af4654894e0d180e49b3cce
Jordi Sala Morales <jordism91@gmail.com>
2018-03-28 10:37:26 +0200

    Update default settings and prod settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -19,6 +19,12 @@ $config_directories = [
   'sync' => '../config/base',
 ];
 
+$config['system.logging']['error_level'] = 'hide';
+$config['system.performance']['cache']['page']['max_age'] = 900;
+$config['system.performance']['css']['preprocess'] = 1;
+$config['system.performance']['js']['preprocess'] = 1;
+$config['stage_file_proxy.settings']['origin'] = false;
+
 $settings['hash_salt'] = '_2iTYTP4sKXrIutDsoT9ZaNAAAuXC34f5VC6OZjs7HmVPnoRu5q9N5gFFqvn7cgGWSEZwZd29Q';
 $settings['update_free_access'] = FALSE;
 $settings['file_scan_ignore_directories'] = ['node_modules', 'bower_components'];
@@ -28,16 +34,4 @@ $settings['skip_permissions_hardening'] = TRUE;
 $settings['trusted_host_patterns'] = ['^drupal\.local$'];
 $settings['install_profile'] = 'minimal';
 
-// DEV ONLY
-assert_options(ASSERT_ACTIVE, TRUE);
-\Drupal\Component\Assertion\Handle::register();
-
-$config['system.performance']['css']['preprocess'] = FALSE;
-$config['system.performance']['js']['preprocess'] = FALSE;
-$config['system.logging']['error_level'] = 'verbose';
-
-$settings['cache']['bins']['render'] = 'cache.backend.null';
-$settings['cache']['bins']['discovery_migration'] = 'cache.backend.memory';
-$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
-$settings['extension_discovery_scan_tests'] = FALSE;
+include $app_root . '/' . $site_path . '/development.settings.php';
