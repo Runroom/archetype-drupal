a3e52452106b5169b93b2568af5f7a86283cd6ab
Aitor Errasti <aitor@runroom.com>
2018-03-28 12:11:03 +0200

    Drupal config modified

--- web/modules/custom/runroom/src/Controller/HomeController.php
+++ web/modules/custom/runroom/src/Controller/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends ControllerBase {
   public function show() {
     return [
       '#type' => 'markup',
-      '#markup' => $this->t("don't hassel the hoff!"),
+      '#markup' => $this->t("Do or do not, there is no try."),
     ];
   }
 
