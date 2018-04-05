2c52a8e4b5980c089bf9b8331cb99b4ec504918a
Jorge <jagundez@runroom.com>
2018-04-05 16:25:13 +0200

    Deploy script without commands of a drupal/drush_language contrib module

--- import-translations.bash
+++ import-translations.bash
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-# drush language-import --langcode=es /vagrant/web/sites/custom_translations/es.po
-
-# BASEDIR=$(dirname "$0")
-# echo "$BASEDIR"
-# echo 'here'
-
 translation_dir=$(pwd)/web/sites/custom_translations
-# translation_dir_es=$translation_dir/es.po
-
-
-
-# drush language-import --langcode=es $translation_dir_es
-
 
 for entry in "$translation_dir"/*
 do
@@ -20,5 +8,5 @@ do
   echo Import translations for $entry
   echo -----------------------
 
-  drush language-import --langcode=es $entry
+  # drush language-import --langcode=es $entry
 done
