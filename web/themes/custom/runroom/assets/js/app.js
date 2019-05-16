2b5dd3a6b11b2e78c6ea33a4c5a52d8e36ffeb8d
Jose <jose@runroom.com>
2019-05-16 16:09:47 +0200

    Iterates app js

--- web/themes/custom/runroom/assets/js/app.js
+++ web/themes/custom/runroom/assets/js/app.js
@@ -1,15 +1,15 @@
 import { scrollTo, events, touchable } from '@runroom/purejs';
-import fastclick from 'fastclick';
-import svg4everybody from 'svg4everybody';
 
 // In order to keep readability and maintainability on bigger projects
 // we recommend to use module import method and import it as needed.
-import './helpers/polyfills';
 import cookies from './components/cookies';
+import lazyLoadImages from './components/lazyLoadImages';
+import './helpers/polyfills';
 
 touchable();
-fastclick.attach(document.body);
-svg4everybody();
+lazyLoadImages();
+
+document.documentElement.classList.remove('no-js');
 
 events.onDocumentReady(() => {
   const anchor = document.querySelector('.js-anchor');
