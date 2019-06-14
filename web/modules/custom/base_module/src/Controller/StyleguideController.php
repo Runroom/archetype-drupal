a49001b847dd666e844236dcfd06457db1655a7b
Jordi Sala Morales <jordism91@gmail.com>
2019-06-14 09:44:59 +0000

    Merged in feature/add-php-cs-fixer (pull request #5)
    Add PHP CS Fixer
    
    Approved-by: Jorge Agundez Medina <jagundez@runroom.com>
    Approved-by: Aitor Errasti <aitor@runroom.com>
    Approved-by: Jordi Sala Morales <jordism91@gmail.com>
    Approved-by: Adrián Señor <adrian@runroom.com>

--- web/modules/custom/base_module/src/Controller/StyleguideController.php
+++ web/modules/custom/base_module/src/Controller/StyleguideController.php
@@ -4,13 +4,14 @@ namespace Drupal\base_module\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-class StyleguideController extends ControllerBase {
-
-  public function show() {
-    return [
-      '#theme' => 'styleguide',
-      '#title' => $this->t('ESADE Exed Styleguide'),
-      '#text' => $this->t('Hello Styleguide!'),
-    ];
-  }
+class StyleguideController extends ControllerBase
+{
+    public function show()
+    {
+        return [
+            '#theme' => 'styleguide',
+            '#title' => $this->t('ESADE Exed Styleguide'),
+            '#text' => $this->t('Hello Styleguide!'),
+        ];
+    }
 }
