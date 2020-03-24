b93535bb438457befbf5997048ade91855dad599
Jordi <jordism91@gmail.com>
2020-03-24 13:31:16 +0100

    Pass cs-fixer, fix phpunit

--- web/sites/custom.settings.php
+++ web/sites/custom.settings.php
@@ -18,16 +18,16 @@ $databases = [
 $settings['trusted_host_patterns'] = ['localhost'];
 $settings['hash_salt'] = '308d47268412acc4fe69f0afaf00b3e63cc7c69f6d94c62d4530d045cd911839';
 
-assert_options(ASSERT_ACTIVE, TRUE);
+\assert_options(ASSERT_ACTIVE, true);
 \Drupal\Component\Assertion\Handle::register();
 
 $config['system.performance']['cache']['page']['max_age'] = 0;
-$config['system.performance']['css']['preprocess'] = FALSE;
-$config['system.performance']['js']['preprocess'] = FALSE;
+$config['system.performance']['css']['preprocess'] = false;
+$config['system.performance']['js']['preprocess'] = false;
 $config['system.logging']['error_level'] = 'verbose';
 
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['discovery_migration'] = 'cache.backend.memory';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 $settings['cache']['bins']['page'] = 'cache.backend.null';
-$settings['extension_discovery_scan_tests'] = FALSE;
+$settings['extension_discovery_scan_tests'] = false;
