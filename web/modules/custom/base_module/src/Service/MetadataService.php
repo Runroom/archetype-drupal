da0c0598e50d8c261f9fab5e913a1ecc06499bde
Jordi <jordism91@gmail.com>
2020-03-26 16:59:07 +0100

    fix cs

--- web/modules/custom/base_module/src/Service/MetadataService.php
+++ web/modules/custom/base_module/src/Service/MetadataService.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\base_module\Service;
 
+use Drupal\Core\Config\ConfigFactory;
+use Symfony\Component\HttpFoundation\RequestStack;
+
 class MetadataService
 {
     public const TERMINATION = '...';
@@ -13,6 +16,16 @@ class MetadataService
         'twitter:title' => 70,
         'twitter:description' => 200,
     ];
+    protected $requestStack;
+    protected $configFactory;
+
+    public function __construct(
+        RequestStack $requestStack,
+        ConfigFactory $configFactory
+    ) {
+        $this->requestStack = $requestStack;
+        $this->configFactory = $configFactory;
+    }
 
     public function truncateMetadata(array &$page): void
     {
@@ -32,9 +45,9 @@ class MetadataService
         string $description,
         string $imageUrl = null
     ): void {
-        $url = \Drupal::request()->getRequestUri();
+        $url = $this->requestStack->getCurrentRequest()->getRequestUri();
 
-        $title = $title . ' | ' . \Drupal::config('system.site')->get('name');
+        $title = $title . ' | ' . $this->configFactory->get('system.site')->get('name');
         $description = \strip_tags($description);
 
         $metadata = [
