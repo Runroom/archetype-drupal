62cbb3dce019e5546126b430d575c9a801268751
Jordi Sala Morales <jordism91@gmail.com>
2020-03-26 10:59:20 +0100

    Use drush for config import and export (#36)

---
+++ drush/Commands/PreConfigImportCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drush\Commands;
+
+class PreConfigImportCommand extends DrushCommands
+{
+    /**
+     * @hook pre-command config:import
+     */
+    public function setUuid()
+    {
+        $staticUuidIsSet = \Drupal::state()->get('static_uuid_is_set');
+
+        if (!$staticUuidIsSet) {
+            $configFactory = \Drupal::configFactory();
+            $configFactory->getEditable('system.site')->set('uuid', '60d59ddd-0588-4020-9dae-405d5aa61f10')->save();
+
+            $this->output()->writeln('Setting the correct UUID for this project: done.');
+
+            \Drupal::state()->set('static_uuid_is_set', 1);
+        }
+    }
+}
