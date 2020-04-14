c7086ddeb1cdb21952968de0e2b025e324c4eee6
Jordi <jordism91@gmail.com>
2020-04-14 11:10:50 +0200

    Fix media lazyload on drupal backend

---
+++ web/modules/custom/base_module/src/Theme/EntityEmbedPreviewThemeNegotiator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\base_module\Theme;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+
+class EntityEmbedPreviewThemeNegotiator implements ThemeNegotiatorInterface
+{
+    protected $user;
+    protected $configFactory;
+    protected $entityTypeManager;
+
+    public function __construct(
+        AccountInterface $user,
+        ConfigFactoryInterface $configFactory,
+        EntityTypeManagerInterface $entityTypeManager
+    ) {
+        $this->user = $user;
+        $this->configFactory = $configFactory;
+        $this->entityTypeManager = $entityTypeManager;
+    }
+
+    public function applies(RouteMatchInterface $routeMatch): bool
+    {
+        return $routeMatch->getRouteName() === 'media.filter.preview'
+            && $this->entityTypeManager->hasHandler('user_role', 'storage')
+            && $this->user->hasPermission('view the administration theme');
+    }
+
+    public function determineActiveTheme(RouteMatchInterface $routeMatch): ?string
+    {
+        return $this->configFactory->get('system.theme')->get('admin');
+    }
+}
