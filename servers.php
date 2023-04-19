7cb1126dfcb56b54846617f0d88bfad8de32fa98
Joshua <joshua@runroom.com>
2023-04-19 11:38:14 +0200

    DEPLOYER_HOST misconfig

--- servers.php
+++ servers.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Deployer;
 
 host('drupal.runroom.dev')
-    ->setHostname(getenv('DEPLOYER_HOST'))
     ->setDeployPath('~/drupal.runroom.dev')
     // labelling with "stage" => "staging" will reset database and load doctrine fixtures
     ->setLabels(['stage' => 'staging'])
