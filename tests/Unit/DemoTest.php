b571358e15155327991bf14af33a8ef3ff0ab53c
Jordi Sala Morales <jordism91@gmail.com>
2024-02-14 15:19:42 +0100

    Update Drupal to 10.2 (#2545)
    * Remove Swiftmailer
    
    * Update to Drupal 10.2
    
    * Configure Symfony mailer
    
    * Update rector
    
    * Update non modules deps
    
    * Update rest of modules

--- tests/Unit/DemoTest.php
+++ tests/Unit/DemoTest.php
@@ -8,10 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 class DemoTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function itWorks(): void
+    public function testItWorks(): void
     {
         static::assertTrue(true);
     }
